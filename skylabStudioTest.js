0da17837d18eec2ef7c8322cc382def691366041
2lives <brandon@skylabtech.ai>
2023-10-20 13:52:22 -0700

    remove skylabstudiotest.js

--- skylabStudioTest.js
+++
@@ -1,9 +0,0 @@
-const skylabStudio = require("./lib/skylabStudio");
-
-const client = skylabStudio("16V7LPczUNXb6cdY7V15G5s5");
-
-const logResult = (err, status, result) => {
-  console.log("resp", err, status, result);
-};
-
-client.listProfiles({}, logResult);
